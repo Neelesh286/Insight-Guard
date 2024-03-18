3462fe7824f1131e9218dbd7c85def4587981cd0
Younus-Saberi <younussaberi@gmail.com>
2024-03-17 21:21:13 -0700

    Correct MODEL Prediction 🚀

--- backend/modelapi/serializers.py
+++ backend/modelapi/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import UploadedImage
 from PIL import Image
+from pathlib import Path
 from .prediction import Predict
 
 class UploadedImageSerializer(serializers.ModelSerializer):
@@ -8,13 +9,10 @@ class UploadedImageSerializer(serializers.ModelSerializer):
         model = UploadedImage
         fields = ('id', 'image', 'uploaded_at')
     
-    def validate_image(self,value):
+    def validate_image(self, value):
         try:
             img = Image.open(value)
             print(f'Image is of type {value}')
-            predictor = Predict()  # Instantiate the Predict class
-            result = predictor.prediction(img)
-            print('===============', result, sep='\n')
         except Exception as e:
             raise serializers.ValidationError(e)
         
@@ -26,5 +24,14 @@ class UploadedImageSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         image = validated_data.pop('image')
-        instance = UploadedImage.objects.create(image= image, **validated_data)
+        instance = UploadedImage.objects.create(image=image, **validated_data)
+        # Get the path of the saved image
+        saved_image_path = instance.image.path
+        # Now you can pass saved_image_path to your predictor function
+        # Instantiate the Predict class
+        predictor = Predict()  
+        saved_img = Image.open(saved_image_path)
+        print(f'========={saved_image_path}=========')
+        saved_image_path_obj = Path(saved_image_path)
+        result = predictor.prediction(saved_image_path_obj)
         return instance
