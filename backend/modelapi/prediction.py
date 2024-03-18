da0f8b067f53af3a8b4bf63cc5a4121d35fa8493
Younus-Saberi <younussaberi@gmail.com>
2024-03-17 21:50:59 -0700

    hotfix/prediction

--- backend/modelapi/prediction.py
+++ backend/modelapi/prediction.py
@@ -2,30 +2,37 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from PIL import Image  # Import PIL.Image to use Image.open
 
 class Predict:
-    model_filename = 'glaucoma.h5'
-    model_path = os.path.join(os.path.dirname(__file__), '..', model_filename)
+    model_filename = 'glau.h5'
+    model_path = os.path.join(os.path.dirname(__file__), model_filename)
 
     def __init__(self):
         print("Prediction model initialization")
 
-    def prediction(self, input_data):
+    def prediction(self, input_data:Image.Image):
         try:
             # Load the model
             model = tf.keras.models.load_model(self.model_path)
             print("Model loaded successfully from:", self.model_path)
 
-            # Make predictions using the loaded model
-            input_data_resized = image.array_to_img(input_data.resize((240, 240)))
-            input_data_array = image.img_to_array(input_data_resized)
-            input_data_array = np.expand_dims(input_data_array, axis=0)
-            predictions = model.predict(input_data_array)
+            # Open the image file
+            img = Image.open(input_data)
 
-            if predictions[0][0] != 1:
-                return "Glaucoma"
+            # Resize the input image
+            img = image.load_img(input_data, target_size=(256, 256))
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            resized_img = tf.image.resize(img_array, (256, 256))
+            # Perform prediction
+            result = model.predict(resized_img / 255.0)
+            print(result)
+
+            if result < 0.5:
+                print(f'Predicted class is Glaucoma')
             else:
-                return "Normal"
+                print(f'Predicted class is Normal')
 
         except Exception as e:
             print(e)
