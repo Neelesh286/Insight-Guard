6151f727b68d7226e8f599771fd946cb1e28600a
Younus-Saberi <younussaberi@gmail.com>
2024-03-31 10:03:01 -0700

    :sparkles: ADD FrontEnd Integration

---
+++ frontend/components/ImageProcessing.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ImageProcessingComponent = () => {
+  const [postResponse, setPostResponse] = useState(null);
+
+  const handlePostClick = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/image-process/');
+      setPostResponse(response.data.status);
+    } catch (error) {
+      console.error('Error posting image:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handlePostClick}>Calculate CDR Ratio</button>
+      {postResponse && (
+        <div>
+          <h3>Your Cup Disc Ratio Stats:</h3>
+          <p>ID: {postResponse.id}</p>
+          <p>Uploaded Image: {postResponse.uploaded_image}</p>
+          <p>Disc Area: {postResponse.disc_area}</p>
+          <p>Cup Area: {postResponse.cup_area}</p>
+          <p>Cup/Disc Ratio: {postResponse.cupdisc_ratio}</p>
+          <img src={postResponse.s3_link} alt="Processed Image" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageProcessingComponent;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ImageUploadComponent from '../components/ImageUploadComponent.jsx'
+import ImageProcessingComponent from '../components/ImageProcessing.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <ImageUploadComponent />
+    <ImageProcessingComponent />
   </React.StrictMode>,
 )
