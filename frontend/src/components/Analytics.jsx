e7ef5ae0f7e6c016990452dd65f9ca651a6ddb8b
Younus-Saberi <younussaberi@gmail.com>
2024-04-02 23:44:48 +0530

    :hammer: Fix/Image Render Vite

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -1,10 +1,10 @@
-import Laptop from '../assets/laptop.jpg'
+//import Laptop from '/assets/laptop.jpg'
 
 const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-      <img className='w-[500px] mx-auto my-4' src={Laptop} alt='Laptop Image' />
+      <img className='w-[500px] mx-auto my-4' src='/assets/laptop.jpg' alt='Laptop Image' />
         <div className='flex flex-col justify-center'>  
           <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
