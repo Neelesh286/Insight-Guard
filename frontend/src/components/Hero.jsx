8dc3344f6391e0c0cc1bfd628628b5daa012905f
Younus-Saberi <younussaberi@gmail.com>
2024-04-03 08:02:38 +0530

    :construction:  Update Navbar, Hero Component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,24 +5,35 @@ const Hero = () => {
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+        YOUR PARTNER IN EYE HEALTH
         </p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-          Grow with data.
+        Protecting your vision.
         </h1>
-        <div className='flex justify-center items-center'>
+        {/* <div className='flex justify-center items-center'>
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
+          Fast, accurate diagnosis for
           </p>
           <ReactTyped
-            strings={['BTB', 'BTC', 'SASS']}
+            strings={['Glaucoma Awareness', 'Eye Health', 'Vision']}
             typeSpeed={120}
             backSpeed={140}
             loop
             className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
           />
-        </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        </div> */}
+  <p className='md:text-5xl sm:text-4xl text-2xl font-bold py-4 text-white-700'>
+  Fast, accurate diagnosis for
+</p>
+<ReactTyped
+  strings={['Glaucoma Awareness', 'Eye Health', 'Your Vision']}
+  typeSpeed={120}
+  backSpeed={140}
+  loop
+  className='md:text-5xl sm:text-4xl text-2xl font-bold text-[#00df9a] md:pl-4 pl-2 mb-7'
+/>
+
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your eye health to prevent vision loss and maintain clarity of sight.</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
       </div>
     </div>
