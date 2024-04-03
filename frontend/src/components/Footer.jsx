0311243833bb204f338eec048ee24ea0259d2469
Younus-Saberi <younussaberi@gmail.com>
2024-04-03 08:28:40 +0530

    :construction:  Update Front End UI

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-        <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>INSIGHT GUARD.</h1>
+        <p className='py-4'>Discover innovative solutions for managing glaucoma and protecting your vision with advanced software technology and personalized care.</p>
         <div className='flex justify-between md:w-[75%] my-6'>
             <FaFacebookSquare size={30} />
             <FaInstagram size={30} />
@@ -26,12 +26,12 @@ const Footer = () => {
         <h6 className='font-medium text-gray-400'>Solutions</h6>
         <ul>
             <li className='py-2 text-sm'>Analytics</li>
-            <li className='py-2 text-sm'>Marketing</li>
-            <li className='py-2 text-sm'>Commerce</li>
+            {/* <li className='py-2 text-sm'>Marketing</li>
+            <li className='py-2 text-sm'>Commerce</li> */}
             <li className='py-2 text-sm'>Insights</li>
         </ul>
     </div>
-    <div>
+    {/* <div>
         <h6 className='font-medium text-gray-400'>Support</h6>
         <ul>
             <li className='py-2 text-sm'>Pricing</li>
@@ -39,15 +39,15 @@ const Footer = () => {
             <li className='py-2 text-sm'>Guides</li>
             <li className='py-2 text-sm'>API Status</li>
         </ul>
-    </div>
+    </div> */}
     <div>
         <h6 className='font-medium text-gray-400'>Company</h6>
         <ul>
             <li className='py-2 text-sm'>About</li>
             <li className='py-2 text-sm'>Blog</li>
-            <li className='py-2 text-sm'>Jobs</li>
-            <li className='py-2 text-sm'>Press</li>
-            <li className='py-2 text-sm'>Careers</li>
+            {/* <li className='py-2 text-sm'>Jobs</li> */}
+            <li className='py-2 text-sm'>Article</li>
+            {/* <li className='py-2 text-sm'>Careers</li> */}
         </ul>
     </div>
     <div>
